c86c3cb49a43e3526591ab5164b17ffc69569722
groomigroom <googjohn0143@gmail.com>
2026-09-20 14:24:13 +0900

    Update 배열에서_자신이_삽입될_위치_찾기.java

--- java/배열에서_자신이_삽입될_위치_찾기.java
+++ java/배열에서_자신이_삽입될_위치_찾기.java
@@ -1,1 +1,24 @@
-d
+import java.util.*;
+
+public class Main {
+public int findInsertIdx (int array[], int data) {
+int findIdx = -1;
+for(int i = 0; i < array[].length; i++) {
+if (array[i] > data) {
+findIdx = i;
+break;
+}
+}
+if (findIdx == -1) {
+return array[].length;
+} else {
+return findIdx;
+}
+}
+
+public static void main(String[] args) {
+int testArray[] = {};
+int insPos = findInsertIdx(testArray[], 55);
+System.out.println(insPos);
+}
+}
