9814395cb13d77cddca0494642d820a49b1a81d5
kimgroomi <googjohn0143@gmail.com>
2026-09-23 20:49:34 +0900

    Refactor findInsertIdx method formatting

--- java/배열에서_자신이_삽입될_위치_찾기.java
+++ java/배열에서_자신이_삽입될_위치_찾기.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
 public class Main {
-public int findInsertIdx (int[] array, int data) {
-int findIdx = -1;
-for(int i = 0; i < array.length; i++) {
-if (array[i] > data) {
-findIdx = i;
-break;
-}
-}
-if (findIdx == -1) {
-return array.length;
-} else {
-return findIdx;
-}
-}
+  public int findInsertIdx (int[] array, int data) {
+    int findIdx = -1;
+    for(int i = 0; i < array.length; i++) {
+      if (array[i] > data) {
+        findIdx = i;
+        break;
+      }
+    }
+    if (findIdx == -1) {
+      return array.length;
+      } else {
+        return findIdx;
+      }
+    }
 
-public static void main(String[] args) {
-int[] testArray = {};
+    public static void main(String[] args) {
+    int[] testArray = {};
 
-// 1. Main 클래스의 객체(인스턴스)를 생성합니다.
-Main mainInstance = new Main();
+    // 1. Main 클래스의 객체(인스턴스)를 생성합니다.
+    Main mainInstance = new Main();
 
-// 2. 생성한 객체를 통해 메서드를 호출합니다.
-int insPos = mainInstance.findInsertIdx(testArray, 55);
-System.out.println(insPos); // 출력: 0
-}
+    // 2. 생성한 객체를 통해 메서드를 호출합니다.
+    int insPos = mainInstance.findInsertIdx(testArray, 55);
+    System.out.println(insPos); // 출력: 0
+  }
 }
