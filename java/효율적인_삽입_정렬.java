608b3b657f6e67d2f0ac35c4c2ac54b254a8aafe
kimgroomi <googjohn0143@gmail.com>
2026-09-24 20:01:22 +0900

    Update 효율적인_삽입_정렬.java

--- java/효율적인_삽입_정렬.java
+++ java/효율적인_삽입_정렬.java
@@ -1,1 +1,24 @@
-d
+def insertionSort(ary):
+    n = len(ary)
+    for end in range(1, n):
+        for cur in range(end, 0, -1):
+            if (ary[cur-1] > ary[cur]):
+                ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
+
+    return ary
+
+dataAry = [188, 162, 168, 120, 50, 150, 177, 105]
+
+#정렬 전
+print(dataAry)
+dataAry = insertionSort(dataAry)
+#정렬 후
+print(dataAry)
+
+"""
+
+[188, 162, 168, 120, 50, 150, 177, 105]
+[50, 105, 120, 150, 162, 168, 177, 188]
+
+
+"""
