6dab1ba061b8d3d0c066d019aa0d28c833ae7ec6
kimgroomi <googjohn0143@gmail.com>
2026-09-24 19:59:26 +0900

    Update 효율적인_삽입_정렬.py

--- python/효율적인_삽입_정렬.py
+++ python/효율적인_삽입_정렬.py
@@ -1,1 +1,10 @@
-d
+def insertionSort(ary):
+    n = len(ary)
+    for end in range(1, n):
+        for cur in range(end, 0, -1):
+            if (ary[cur-1] > ary[cur]):
+                ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
+
+    return ary
+
+data
