0dd1945c08831a9b122826ad2c3046aa2f34321d
kimgroomi <googjohn0143@gmail.com>
2026-09-24 20:16:58 +0900

    Update 효율적인_삽입_정렬.js

--- javascript/효율적인_삽입_정렬.js
+++ javascript/효율적인_삽입_정렬.js
@@ -3,7 +3,9 @@ const insertionSort = (ary) => {
     for (let end = 1; end < n; end++) {
         for (let cur = end; cur > 0; cur--) {}
             if (ary[cur-1] > ary[cur]) {
-                ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
+                let tmp = ary[cur-1];
+                ary[cur-1] = ary[cur];
+                ary[cur] = tmp;
             }
     }
 
